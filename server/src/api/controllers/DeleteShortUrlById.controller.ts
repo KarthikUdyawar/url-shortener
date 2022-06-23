fd4517b97552aa82bb71988b415021da79880fed
Karthik Udyawar <karthikajitudy@gmail.com>
2022-06-23 17:15:11 +0530

    Added new API to delete data by id
    ### Added
    - Added new controller called DeleteShortUrlById that delete data by id from the database for admin

---
+++ server/src/api/controllers/DeleteShortUrlById.controller.ts
@@ -0,0 +1,33 @@
+import { ShortUrls } from "../models/shortURL.model";
+import { Response } from "express";
+import ErrorHandler from "../middleware/Handler/ErrorHandler";
+import HttpException from "../middleware/Exceptions/HttpException";
+import IMessage from "./interfaces/IMessage";
+import IResponse from "./interfaces/IResponse";
+import IShortUrl from "../models/interfaces/IShortUrl";
+import IRequest from "./interfaces/IRequest";
+import IReqParams from "./interfaces/IReqParams";
+
+const DeleteShortUrlById = async (req: IRequest, res: Response) => {
+  const msg: IMessage = { isSuccessful: false, message: "", info: null };
+  const result: IResponse = { code: -1, data: msg };
+  try {
+    const { id }: IReqParams = req.params;
+    const shortUrls: IShortUrl | null = await ShortUrls.findByIdAndDelete(id);
+
+    if (shortUrls) {
+      msg.isSuccessful = true;
+      result.code = 200;
+      msg.message = `Successfully Deleted URL by id`;
+      msg.info = shortUrls;
+    }
+
+    return res.status(result.code).json(result.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      ErrorHandler(error as HttpException, res);
+    }
+  }
+};
+
+export default DeleteShortUrlById;
--- server/src/api/routes/Admin.routes.ts
+++ server/src/api/routes/Admin.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import DeleteShortUrlById from "../controllers/DeleteShortUrlById.controller";
 import FetchShortUrlsById from "../controllers/FetchShortUrlById.controller";
 import FetchShortUrlsAll from "../controllers/FetchShortUrlsAll.controllers";
 
@@ -7,5 +8,7 @@ const AdminRoutes: Router = Router();
 AdminRoutes.get("/all", FetchShortUrlsAll);
 
 AdminRoutes.get("/:id", FetchShortUrlsById);
+
+AdminRoutes.delete("/delete/:id", DeleteShortUrlById);
 
 export default AdminRoutes;
